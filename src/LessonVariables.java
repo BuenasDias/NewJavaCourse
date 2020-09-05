df751073bf5347f402d048bc6b5bf46ccc81e61d
Maxim Tolstobrov <tmaksim1989@gmail.com>
2020-09-05 20:53:46 +0300

    Create binary operation

--- src/LessonVariables.java
+++ src/LessonVariables.java
@@ -1,7 +1,17 @@
-
-
-
 public class LessonVariables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        int c;
+
+        c = a + b;      // 12
+        c = a - b;      // 8
+        c = a * b;      // 20
+        c = a / b;      // 5
+        c = a % b;      // 0
 
 
+
+    }
 }
